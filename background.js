be61055b331272a7be23a2296be7b1c51e154962
Shafin <sa.fine@outlook.com>
2021-08-11 16:55:45 +0600

    first commit

---
+++ background.js
@@ -0,0 +1,8 @@
+let color = '#A5AAA3';
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({
+        color
+    });
+    console.log('Default background color set to %cgreen', `color: ${color}`);
+});
